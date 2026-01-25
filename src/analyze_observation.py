6739d26f86bc9b5f3c2a6fd9b8623edc7c812060
Nouran Ghaliounji <nouran.ghaliounji@gmail.com>
2026-01-25 21:44:39 +0100

    Add Kafka consumer and rules-based anomaly detection

---
+++ src/analyze_observation.py
@@ -0,0 +1,76 @@
+def analyze_blood_pressure(systolic, diastolic):
+    """
+    Analyse une mesure de pression artérielle
+    et retourne les anomalies détectées.
+    """
+
+    anomalies = []
+
+    # Règles médicales
+    if systolic > 140:
+        anomalies.append("hypertension")
+    if systolic < 90:
+        anomalies.append("hypotension")
+
+    if diastolic > 90:
+        anomalies.append("hypertension")
+    if diastolic < 60:
+        anomalies.append("hypotension")
+
+    return anomalies
+
+
+def extract_bp_from_fhir_observation(observation: dict):
+    """
+    Extrait systolic et diastolic d'une Observation FHIR.
+    On cherche les composants par leur code LOINC:
+    - 8480-6 = systolic
+    - 8462-4 = diastolic
+    """
+
+    systolic = None
+    diastolic = None
+
+    for comp in observation.get("component", []):
+        coding_list = comp.get("code", {}).get("coding", [])
+        if not coding_list:
+            continue
+
+        code = coding_list[0].get("code")
+        value = comp.get("valueQuantity", {}).get("value")
+
+        if code == "8480-6":
+            systolic = value
+        elif code == "8462-4":
+            diastolic = value
+
+    return systolic, diastolic
+
+
+if __name__ == "__main__":
+    # Exemple minimal inspiré de ton générateur
+    observation = {
+        "resourceType": "Observation",
+        "subject": {"reference": "Patient/PAT-9486"},
+        "effectiveDateTime": "2026-01-16T15:42:56.834661Z",
+        "component": [
+            {
+                "code": {"coding": [{"code": "8480-6"}]},
+                "valueQuantity": {"value": 140.0, "unit": "mmHg"},
+            },
+            {
+                "code": {"coding": [{"code": "8462-4"}]},
+                "valueQuantity": {"value": 54.0, "unit": "mmHg"},
+            },
+        ],
+    }
+
+    systolic, diastolic = extract_bp_from_fhir_observation(observation)
+    print("Systolic:", systolic, "Diastolic:", diastolic)
+
+    anomalies = analyze_blood_pressure(systolic, diastolic)
+
+    if anomalies:
+        print("Anomalie détectée :", anomalies)
+    else:
+        print("Pression normale")
